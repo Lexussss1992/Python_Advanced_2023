8c597ec2cabb8ac26387435c9c936d5745735852
Ivelin <61937094+Lexussss1992@users.noreply.github.com>
2023-04-25 08:47:38 +0200

    Stacks and Queues

--- Lists and queues/Key Revolver.py
+++ Lists and queues/Key Revolver.py
@@ -6,26 +6,30 @@ bullets = list(map(int, input().split()))
 locks = deque(map(int, input().split()))
 inteL_value = int(input())
 count = 0
+count_bullets = 0
 
 while len(locks) > 0:
     if inteL_value > 0:
-        if len(locks) > 0 and len(bullets) > 0:
+        if len(bullets) > 0:
             if bullets[-1] - locks[0] <= 0:
                 print('Bang!')
-                locks.popleft()
                 bullets.pop()
+                locks.popleft()
                 count += 1
-                if count == barrel_size:
-                    print("Reloading!")
+                count_bullets += 1
+                if barrel_size == count and len(bullets) > 0:
+                    print('Reloading!')
                     count = 0
             else:
                 print('Ping!')
                 bullets.pop()
                 count += 1
-                if count == barrel_size:
-                    print("Reloading!")
+                count_bullets += 1
+                if barrel_size == count and len(bullets) > 0:
+                    print('Reloading!')
                     count = 0
         else:
+            print(f"Couldn't get through. Locks left: {len(locks)}")
             break
-    else:
-        break
+else:
+    print(f'{count} bullets left. Earned ${inteL_value - bullet_price * count_bullets}')
