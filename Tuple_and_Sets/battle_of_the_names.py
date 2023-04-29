67f63c7fc72a14a573b2992fc6a585789485a44d
Ivelin <61937094+Lexussss1992@users.noreply.github.com>
2023-04-29 19:12:04 +0200

    Tuples and Sets

--- Tuple_and_Sets/battle_of_the_names.py
+++ Tuple_and_Sets/battle_of_the_names.py
@@ -1,15 +1,21 @@
-even_numbers = set()
-odd_numbers = set()
+import math
+
+even_numbers = []
+odd_numbers = []
 
 for i in range(1, int(input()) + 1):
     sum_letters = 0
     for x in input():
         sum_letters += ord(x)
-    sum_letters = round(sum_letters / i)
+    sum_letters = math.floor(sum_letters / i)
     if sum_letters % 2 == 0:
-        even_numbers.add(sum_letters)
+        even_numbers.append(sum_letters)
     else:
-        odd_numbers.add(sum_letters)
+        odd_numbers.append(sum_letters)
 
-print(even_numbers)
-print(odd_numbers)
+if sum(even_numbers) == sum(odd_numbers):
+    print(", ".join(str(i) for i in set(even_numbers).union(set(odd_numbers))))
+elif sum(odd_numbers) > sum(even_numbers):
+    print(", ".join(str(i) for i in set(odd_numbers).difference(set(even_numbers))))
+elif sum(even_numbers) > sum(odd_numbers):
+    print(", ".join(str(i) for i in set(even_numbers).symmetric_difference(set(odd_numbers))))
