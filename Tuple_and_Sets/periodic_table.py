9e9dd182cdb109b93f6d7ab29a423a46c22818e9
Ivelin <61937094+Lexussss1992@users.noreply.github.com>
2023-04-29 10:06:56 +0200

    Tuples and Sets

--- Tuple_and_Sets/periodic_table.py
+++ Tuple_and_Sets/periodic_table.py
@@ -1,3 +1,15 @@
 n = int(input())
-elements = [input().split() for i in range(n)]
+elements = tuple(input().split() for i in range(n))
+el_list = []
+el_set = set()
 
+for i in elements:
+    for x in i:
+        el_list.append(x)
+
+for i in el_list:
+    el_set.add(i)
+
+
+for i in el_set:
+    print(i)
