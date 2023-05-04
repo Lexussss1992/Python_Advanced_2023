76c8e4d79cd7e9942d916edfb8d1f9f3278e078c
Ivelin <61937094+Lexussss1992@users.noreply.github.com>
2023-05-04 07:59:05 +0200

    Tuples and Sets

--- Stacks, Queues, Tuples, and Sets/milkshake.py
+++ Stacks, Queues, Tuples, and Sets/milkshake.py
@@ -2,18 +2,26 @@ from collections import deque
 
 chocolate = [int(x) for x in input().split(',')]
 milk_cups = deque([int(i) for i in input().split(',')])
-n = 5
+milkshakes = 0
 
-for i in range(n):
-    if len(chocolate) > 0  or len(milk_cups) > 0:
+while True:
+    if len(chocolate) > 0 and len(milk_cups) > 0:
         result = chocolate[-1] - milk_cups[0]
 
         if result == 0:
             chocolate.pop()
             milk_cups.popleft()
+            milkshakes += 1
         else:
-            milk_cups.append(milk_cups[0])
-            milk_cups.popleft()
-            chocolate.pop()
+            if chocolate[-1] <= 0:
+                chocolate.pop()
+            elif milk_cups[0] <= 0:
+                milk_cups.append(milk_cups[0])
+                milk_cups.popleft()
+                chocolate[-1] -= 5
+    elif milkshakes == 5:
+        break
+    else:
+        break
 print(chocolate)
 print(milk_cups)
