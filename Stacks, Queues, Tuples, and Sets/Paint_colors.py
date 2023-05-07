e476b0e641bbdd83b3892482f61ced193a83da6d
Ivelin <61937094+Lexussss1992@users.noreply.github.com>
2023-05-07 22:27:04 +0200

    Tuples and Sets

--- Stacks, Queues, Tuples, and Sets/Paint_colors.py
+++ Stacks, Queues, Tuples, and Sets/Paint_colors.py
@@ -6,26 +6,37 @@ secondary_colors = ['orange', 'purple', 'green']
 colors = []
 
 while string:
-    first_substring = string.pop(0)
-    last_substring = string.pop()
-    color = first_substring + last_substring
+    first_substring = ''
+    last_substring = ''
+    reversed_color = ''
+    if len(string) > 1:
+        first_substring = string.pop(0)
+        last_substring = string.pop()
+        color = first_substring + last_substring
+        reversed_color = last_substring + first_substring
+    else:
+        color = string.pop()
 
     if color in main_colors:
         colors.append(color)
-    elif color in secondary_colors:
-        if color == 'redyellow':
+    elif reversed_color in main_colors:
+        colors.append(reversed_color)
+    elif color in secondary_colors or reversed_color in secondary_colors:
+        if color == 'orange' or reversed_color == 'orange':
             colors.append('orange')
-        elif color == 'redblue':
+        elif color == 'purple' or reversed_color == 'purple':
             colors.append('purple')
-        elif color == 'yellowblue':
+        elif color == 'green' or reversed_color == 'green':
             colors.append('green')
     else:
-        if len(first_substring[:-1]) == 0:
+        if len(first_substring[:-1]) == 0 and len(last_substring[:-1]) == 0:
             continue
+        elif len(first_substring[:-1]) == 0 and len(last_substring[:-1]) > 0:
+            string.insert(int(len(string)/2), last_substring[:-1])
+        elif len(first_substring[:-1]) > 0 and len(last_substring[:-1]) == 0:
+            string.insert(int(len(string)/2), first_substring[:-1])
         else:
-            string.append()
-        if len(last_substring[:-1]) == 0:
-            continue
-        else:
-            pass
+            string.insert(int(len(string) / 2), first_substring)
+            string.insert(int(len(string) / 2 + 1), last_substring)
+
 print(colors)
