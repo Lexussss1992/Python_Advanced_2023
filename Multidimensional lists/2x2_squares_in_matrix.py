3c276fcfb9f708ce759b51a3ad9a955064435317
Ivelin <61937094+Lexussss1992@users.noreply.github.com>
2023-05-11 13:13:35 +0200

    Tuples and Sets

--- Multidimensional lists/2x2_squares_in_matrix.py
+++ Multidimensional lists/2x2_squares_in_matrix.py
@@ -1,19 +1,26 @@
-rows, columns = [int(x) for x in input().split()]
-matrix = []
-twoBYtwo_matrices = 0
-n = 2
-for row in range(rows):
-    column = [str(x) for x in input().split()]
-    matrix.append(column)
+rows, cols = [int(n) for n in input().split()]
 
-for row in range(rows):
-    for column in range(columns - 1):
-        if matrix[row][column] == matrix[row][column + 1] and matrix[row + 1][column] == matrix[row + 1][column + 1]:
-            twoBYtwo_matrices += 1
-        else:
-            continue
+matrix = [[x for x in input().split()] for u in range(rows)]
 
-print(twoBYtwo_matrices)
+squares = []
+
+for i in range(rows - 1):
+    for j in range(cols - 1):
+        square = [
+            [matrix[i][j]], [matrix[i][j + 1]],
+            [matrix[i + 1][j]], [matrix[i + 1][j + 1]],
+        ]
+        squares.append(square)
+
+overall_count = 0
+for item in squares:
+    current_list = []
+    for char in item:
+        current_list.append(char[0])
+    if len(set(current_list)) == 1:
+        overall_count += 1
+
+print(overall_count)
 
 # 3 4
 # A B B D
