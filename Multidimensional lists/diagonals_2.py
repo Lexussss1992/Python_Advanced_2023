6854faacd1e62fd8581dd4c97259393342e9588d
Ivelin <61937094+Lexussss1992@users.noreply.github.com>
2023-05-11 09:15:06 +0200

    Tuples and Sets

--- Multidimensional lists/diagonals_2.py
+++ Multidimensional lists/diagonals_2.py
@@ -1,9 +1,18 @@
 n = int(input())
 matrix = []
 
-for i in range(n):
-    matrix.append([])
-    for j in range(1, n + 1):
-        matrix[i].append(j + (i * 3))
+# for i in range(n):
+#     matrix.append([])
+#     for j in range(1, n + 1):
+#         matrix[i].append(j + (i * 3))
 
-print(matrix)
+for row in range(n):
+    lines = [int(x) for x in input().split(', ')]
+    matrix.append(lines)
+
+primary_diagonal = 0
+secondary_diagonal = 0
+
+for i in range(len(matrix)):
+    for j in range(len(matrix)):
+        primary_diagonal += matrix[i] +
