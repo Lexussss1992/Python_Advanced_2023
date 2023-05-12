40b2127f780edd1b12f7e3f36cc474560f40991a
Ivelin <61937094+Lexussss1992@users.noreply.github.com>
2023-05-12 22:02:10 +0200

    Multidimensional sets

--- Multidimensional lists/matrix_shuffling.py
+++ Multidimensional lists/matrix_shuffling.py
@@ -8,20 +8,16 @@ command, coordinates = user_input.popleft(), [int(x) for x in user_input]
 
 while command != 'END':
     if command == 'swap':
-        if coordinates[0] <= cols and coordinates[1] <= rows and coordinates[2] <= cols and coordinates[3] <= rows:
+        if coordinates[0] <= cols and coordinates[1] <= rows and coordinates[2] <= cols and coordinates[3] <= rows and command:
             el1 = matrix[coordinates[0]][coordinates[1]]
             el2 = matrix[coordinates[2]][coordinates[3]]
             matrix[coordinates[2]][coordinates[3]] = el1
             matrix[coordinates[0]][coordinates[1]] = el2
-            print(matrix)
+            for index, item in enumerate(matrix, start=1):
+                print(*item, end='' if index % 1 else '\n')
         else:
-            print('Invalid input !')
+            print('Invalid input!')
+    else:
+        print('Invalid input!')
     user_input = deque([x for x in input().split()])
     command, coordinates = user_input.popleft(), [int(x) for x in user_input]
-# 2 3
-# 1 2 3
-# 4 5 6
-# swap 0 0 1 1
-# swap 10 9 8 7
-# swap 0 1 1 0
-# END
