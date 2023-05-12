f35f5ae0afec98a21acb1dbfff4d3757240ee04e
Ivelin <61937094+Lexussss1992@users.noreply.github.com>
2023-05-12 21:29:15 +0200

    Multidimensional sets

--- Multidimensional lists/matrix_shuffling.py
+++ Multidimensional lists/matrix_shuffling.py
@@ -4,18 +4,20 @@ rows, cols = [int(x) for x in input().split()]
 
 matrix = [[x for x in input().split()] for u in range(rows)]
 user_input = deque([x for x in input().split()])
-matrix_copy = matrix.copy()
+command, coordinates = user_input.popleft(), [int(x) for x in user_input]
 
-while user_input != 'END':
-    command, coordinates = user_input.popleft(), [int(x) for x in user_input]
+while command != 'END':
     if command == 'swap':
-        el1 = matrix[coordinates[0]][coordinates[1]]
-        el2 = matrix[coordinates[2]][coordinates[3]]
-        matrix_copy[coordinates[2]][coordinates[3]] = el1
-        matrix_copy[coordinates[0]][coordinates[1]] = el2
-        print(matrix_copy)
+        if coordinates[0] <= cols and coordinates[1] <= rows and coordinates[2] <= cols and coordinates[3] <= rows:
+            el1 = matrix[coordinates[0]][coordinates[1]]
+            el2 = matrix[coordinates[2]][coordinates[3]]
+            matrix[coordinates[2]][coordinates[3]] = el1
+            matrix[coordinates[0]][coordinates[1]] = el2
+            print(matrix)
+        else:
+            print('Invalid input !')
     user_input = deque([x for x in input().split()])
-
+    command, coordinates = user_input.popleft(), [int(x) for x in user_input]
 # 2 3
 # 1 2 3
 # 4 5 6
