74152c63dda094cd36d3610b368dac49f5a8835d
Ivelin <61937094+Lexussss1992@users.noreply.github.com>
2023-05-16 22:25:25 +0200

    Multidimensional sets

--- Multidimensional lists/matrix_modification.py
+++ Multidimensional lists/matrix_modification.py
@@ -1,9 +1,32 @@
-rows = int(input())
+def check_coordinates(coordinates):
+    coordinates = [int(x) for x in coordinates]
+    if 0 <= coordinates[0] <= len(matrix) - 1 and 0 <= coordinates[1] <= len(matrix) - 1:
+        return True
+
+
+def modify_matrix(matrix, command, coordinates):
+    if command == 'Add':
+        matrix[coordinates[0]][coordinates[1]] += coordinates[2]
+    elif command == 'Subtract':
+        matrix[coordinates[0]][coordinates[1]] -= coordinates[2]
+    return matrix
+
+n = int(input())
 matrix = []
 
-for i in range(rows):
+for i in range(n):
     matrix.append([int(x) for x in input().split()])
 
+command, *coordinates = [x for x in input().split()]
+coordinates = [int(x) for x in coordinates]
 
+while command != 'END':
+    coordinates = [int(x) for x in coordinates]
+    if check_coordinates(coordinates):
+        modify_matrix(matrix, command, coordinates)
+    else:
+        print('Invalid coordinates')
+    command, *coordinates = [x for x in input().split()]
 
-while command == 'END':
+for el in matrix:
+    print(*el, end='\n')
