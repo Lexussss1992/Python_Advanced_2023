dec74cb9f70da299e8cee3a31586046a1611e1a4
Ivelin <61937094+Lexussss1992@users.noreply.github.com>
2023-05-16 08:07:59 +0200

    Multidimensional sets

--- Multidimensional lists/flatten_lists.py
+++ Multidimensional lists/flatten_lists.py
@@ -1,4 +1,5 @@
 use_input = input().split('|')
 
 for i in range(len(use_input) - 1, -1, -1):
-    print(use_input[i], end=' ')
+    # print(use_input[i].split(' '), end=' ')
+    [print(x, end=' ') if x != '' else '' for x in use_input[i].split(' ')]
