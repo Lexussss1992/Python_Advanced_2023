d1aafc3310f0aa4363042f6e31faa1936ad63737
Ivelin <61937094+Lexussss1992@users.noreply.github.com>
2023-05-16 08:27:44 +0200

    Multidimensional sets

--- Multidimensional lists/matrix_modification.py
+++ Multidimensional lists/matrix_modification.py
@@ -2,5 +2,8 @@ rows = int(input())
 matrix = []
 
 for i in range(rows):
-    matrix.append([x for x in input().split()])
-print(matrix)
+    matrix.append([int(x) for x in input().split()])
+
+
+
+while command == 'END':
