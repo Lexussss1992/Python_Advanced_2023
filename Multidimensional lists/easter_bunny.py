bd45ce6bb0e0d10a59c52b4d59540b42847f5a94
Ivelin <61937094+Lexussss1992@users.noreply.github.com>
2023-05-18 18:04:39 +0200

    Multidimensional sets

--- Multidimensional lists/easter_bunny.py
+++ Multidimensional lists/easter_bunny.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 n = int(input())
 matrix = [input().split() for _ in range(n)]
 bunny_coordinates = []
@@ -17,38 +19,47 @@ left_list = []
 down = 0
 down_list = []
 
-for x in range(bunny_coordinates[0], 0, -1): #up
+dict = {
+    'up': [0, []],
+    'down': 0,
+    'left': 0,
+    'right': 0
+}
+
+for x in range(bunny_coordinates[0], -1, -1): #up
     if matrix[x][bunny_coordinates[1]] != 'B':
         if matrix[x][bunny_coordinates[1]] == 'X':
             break
         else:
-            up += int(matrix[x][bunny_coordinates[1]])
-            up_list.append([x, bunny_coordinates[1]])
-
-for i in range(bunny_coordinates[1], len(matrix)): #right
-    if matrix[bunny_coordinates[0]][i] != 'B':
-        if matrix[bunny_coordinates[0]][i] == 'X':
-            break
-        else:
-            right += int(matrix[bunny_coordinates[0]][i])
-            right_list.append([bunny_coordinates[0], i])
-
-
-for i in range(bunny_coordinates[1], 0, -1): #left
-    if matrix[bunny_coordinates[0]][i] != 'B':
-        if matrix[bunny_coordinates[0]][i] == 'X':
-            break
-        else:
-            left += int(matrix[bunny_coordinates[0]][i])
-            left_list.append([bunny_coordinates[0], i])
-
-for x in range(bunny_coordinates[0], n): #down
-    if matrix[x][bunny_coordinates[1]] != 'B':
-        if matrix[x][bunny_coordinates[1]] == 'X':
-            break
-        else:
-            down += int(matrix[x][bunny_coordinates[1]])
-            down_list.append([x, bunny_coordinates[1]])
+            for key, value in dict.items():
+                if key == 'up':
+                    value[1].append([x, bunny_coordinates[1]])
+                    value[0] += int(matrix[x][bunny_coordinates[1]])
+print(dict)
+# for i in range(bunny_coordinates[1], len(matrix)): #right
+#     if matrix[bunny_coordinates[0]][i] != 'B':
+#         if matrix[bunny_coordinates[0]][i] == 'X':
+#             break
+#         else:
+#             right += int(matrix[bunny_coordinates[0]][i])
+#             right_list.append([bunny_coordinates[0], i])
+#
+#
+# for i in range(bunny_coordinates[1], 0, -1): #left
+#     if matrix[bunny_coordinates[0]][i] != 'B':
+#         if matrix[bunny_coordinates[0]][i] == 'X':
+#             break
+#         else:
+#             left += int(matrix[bunny_coordinates[0]][i])
+#             left_list.append([bunny_coordinates[0], i])
+#
+# for x in range(bunny_coordinates[0], n): #down
+#     if matrix[x][bunny_coordinates[1]] != 'B':
+#         if matrix[x][bunny_coordinates[1]] == 'X':
+#             break
+#         else:
+#             down += int(matrix[x][bunny_coordinates[1]])
+#             down_list.append([x, bunny_coordinates[1]])
 
 
 
