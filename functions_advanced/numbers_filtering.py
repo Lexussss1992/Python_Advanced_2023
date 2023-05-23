358b38a07943976d758730efca4cc595991dce15
Ivelin <61937094+Lexussss1992@users.noreply.github.com>
2023-05-23 08:47:25 +0200

    Functions Advanced

---
+++ functions_advanced/numbers_filtering.py
@@ -0,0 +1,19 @@
+def even_odd_filter(**args):
+    dict = {
+
+    }
+    for key, value in args.items():
+        if key == 'even':
+            for x in value:
+                if x % 2 == 0:
+                    return value
+        elif key == 'odd':
+            for x in value:
+                if x % 2 != 0:
+                    return value
+
+
+print(even_odd_filter(
+odd=[1, 2, 3, 4, 10, 5],
+even=[3, 4, 5, 7, 10, 2, 5, 5, 2],
+))
