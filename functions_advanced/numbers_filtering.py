3721e61baa91995b02d1b91f13fb342c14a93ad3
Ivelin <61937094+Lexussss1992@users.noreply.github.com>
2023-05-23 18:42:23 +0200

    Functions Advanced

--- functions_advanced/numbers_filtering.py
+++ functions_advanced/numbers_filtering.py
@@ -1,14 +1,26 @@
-def even_odd_filter(**args):
-    for key, value in args.items():
+def even_odd_filter(**kwargs):
+    dict = {}
+    for key, value in kwargs.items():
         if key == 'even':
-            for x in value:
-                return args.values() if x % 2 == 0 else None
+            dict[key] = []
+            for i in value:
+                if i % 2 == 0:
+                    dict[key].append(i)
         elif key == 'odd':
-            for x in value:
-                return args.values() if x % 2 != 0 else None
+            dict[key] = []
+            for i in value:
+                if i % 2 != 0:
+                    dict[key].append(i)
+    return dict
 
 
 print(even_odd_filter(
 odd=[1, 2, 3, 4, 10, 5],
-even=[3, 4, 5, 7, 10, 2, 5, 5, 2],
+even=[3, 4, 5, 7, 10, 2, 5, 5, 2]
 ))
+
+# odd_result = {key: [idx for idx in val if not idx % 2]
+#               for key, val in kwargs.items()}
+# even_result = {key: [idx for idx in val if not idx == 2]
+#                for key, val in kwargs.items()}
+# return odd_result, even_result
