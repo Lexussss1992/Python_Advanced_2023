3b2dbf900b333331729549899e983a6a677f01d4
Ivelin <61937094+Lexussss1992@users.noreply.github.com>
2023-05-25 07:22:24 +0200

    Functions Advanced

--- functions_advanced/age_assignment.py
+++ functions_advanced/age_assignment.py
@@ -1,6 +1,6 @@
 def age_assignment(*args, **kwargs):
     result = []
-    for key, value in kwargs.items():
+    for key, value in sorted(kwargs.items(), key=lambda x: x[0]):
         for x in args:
             for j in x:
                 if key == j:
@@ -8,6 +8,7 @@ def age_assignment(*args, **kwargs):
     return '\n'.join(result)
 
 
-# print(age_assignment("Peter", "George", G=26, P=19))
+print(age_assignment("Peter", "George", G=26, P=19))
+# print(age_assignment("Amy", "Bill", "Willy", W=36, A=22, B=61))
 
-print(age_assignment("Amy", "Bill", "Willy", W=36, A=22, B=61))
+# sorted(my_dict.items(), key=lambda x: x[0])
