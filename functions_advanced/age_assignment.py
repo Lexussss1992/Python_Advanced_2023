28ca1e813af9c2cb2d660ed5fbe3bf3e1ac7b391
Ivelin <61937094+Lexussss1992@users.noreply.github.com>
2023-05-25 07:10:23 +0200

    Functions Advanced

--- functions_advanced/age_assignment.py
+++ functions_advanced/age_assignment.py
@@ -1,11 +1,13 @@
 def age_assignment(*args, **kwargs):
     result = []
     for key, value in kwargs.items():
-        for x in args[0]:
-            if key == x:
-                result.append(args[0])
-                result.append(value)
-    return result
+        for x in args:
+            for j in x:
+                if key == j:
+                    result.append(f'{x} is {value} years old.')
+    return '\n'.join(result)
 
 
-print(age_assignment("Peter", "George", G=26, P=19))
+# print(age_assignment("Peter", "George", G=26, P=19))
+
+print(age_assignment("Amy", "Bill", "Willy", W=36, A=22, B=61))
