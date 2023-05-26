73bafe4f3580961f6c9dbbe2910914dd11501986
Ivelin <61937094+Lexussss1992@users.noreply.github.com>
2023-05-26 13:40:09 +0200

    Functions Advanced

--- functions_advanced/math_operations.py
+++ functions_advanced/math_operations.py
@@ -1,29 +1,30 @@
-from collections import deque
-
-
 def math_operations(*args, **kwargs):
+    from collections import deque
     deque_args = deque(i for i in args)
-    result = [0, 0, 0, 0]
+    result = []
     while len(deque_args) > 0:
         for key, value in kwargs.items():
             deq_value = deque_args[0]
             if key == 'a':
-                result[0] = deq_value + value
-                kwargs[key] = result[0]
+                kwargs[key] = deq_value + value
             elif key == 's':
-                result[1] = value - deq_value
-                kwargs[key] = result[1]
+                kwargs[key] = value - deq_value
             elif key == 'd':
                 if deq_value != 0:
-                    result[2] = value / deq_value
-                    kwargs[key] = result[1]
+                    kwargs[key] = value / deq_value
             elif key == 'm':
-                result[3] = deq_value * value
-                kwargs[key] = result[3]
+                kwargs[key] = deq_value * value
             deq_value = deque_args.popleft()
             if len(deque_args) <= 0:
                 break
+    sorted_kwargs = sorted(kwargs.items(), key=lambda x: (-x[1], x[0]))
+    for key, value in sorted_kwargs:
+        result.append(f'{key}: {value:.1f}')
 
-    return result, deque_args
+    return '\n'.join(result)
 
-print(math_operations(2.1, 12.56, 0.0, -3.899, 6.0, -20.65, a=1, s=7, d=33, m=15))
+# print(math_operations(2.1, 12.56, 0.0, -3.899, 6.0, -20.65, a=1, s=7, d=33, m=15))
+
+print(math_operations(-1.0, 0.5, 1.6, 0.5, 6.1, -2.8, 80.0, a=0, s=(-2.3), d=0, m=0))
+
+#print(math_operations(6.0, a=0, s=0, d=5, m=0))
