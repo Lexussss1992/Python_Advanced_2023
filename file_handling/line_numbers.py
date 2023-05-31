4c2fa684d81785d0855089fdb85cc363cace4054
Ivelin <61937094+Lexussss1992@users.noreply.github.com>
2023-05-31 08:34:54 +0200

    file_handling

--- file_handling/line_numbers.py
+++ file_handling/line_numbers.py
@@ -5,5 +5,7 @@ with open('text.txt', 'r') as f:
     for line in f.readlines():
         char = ''.join(char for char in line if char not in punctuation and char != ' ' and char != '\n')
         punctuations = ''.join(char for char in line if char in punctuation)
-        print(f'Line {i}: {line}({len(char)})({len(punctuations)})')
+        output = f'Line {i}: {line}({len(char)})({len(punctuations)})'
         i += 1
+        with open('output.txt', 'a') as of:
+            of.writelines(output + '\n')
