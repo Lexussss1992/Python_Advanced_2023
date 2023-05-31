693af92394731a8b875d5122c8a309adbf18c9d1
Ivelin <61937094+Lexussss1992@users.noreply.github.com>
2023-05-31 07:48:20 +0200

    file_handling

--- file_handling/even_lines.py
+++ file_handling/even_lines.py
@@ -1,2 +1,10 @@
 with open('text.txt', 'r') as f:
-    for line in range(len(f.readlines())):
+    i = 0
+    char_remove = ["-", ",", ".", "!", "?"]
+    for line in f.readlines():
+        if i % 2 == 0:
+            for char in char_remove:
+                line = line.replace(char, '@')
+                new_line = [i for i in line.split()]
+            print(*new_line[::-1])
+        i += 1
