807ea4382e39d759866b3470f95e725b28e9cbdc
Ivelin <61937094+Lexussss1992@users.noreply.github.com>
2023-06-12 07:12:12 +0200

    Exam-prep

---
+++ exam_prep/naughty_or_nice.py
@@ -0,0 +1,18 @@
+def naughty_or_nice_list(list, *names, **commands):
+    return list, names, commands
+
+
+
+print(naughty_or_nice_list(
+    [
+        (3, "Amy"),
+        (1, "Tom"),
+        (7, "George"),
+        (3, "Katy"),
+    ],
+    "3-Nice",
+    "1-Naughty",
+    Amy="Nice",
+    Katy="Naughty",
+)
+)
