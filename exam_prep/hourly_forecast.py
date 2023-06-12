029b814b8ba6969cf59eac8f1cf8e0fb7011451b
Ivelin <61937094+Lexussss1992@users.noreply.github.com>
2023-06-12 08:08:12 +0200

    Exam prep

--- exam_prep/hourly_forecast.py
+++ exam_prep/hourly_forecast.py
@@ -3,13 +3,26 @@ def forecast(*args):
     result = []
     for city, forecast in args:
         forecast_dict[city] = forecast
-    sorted_forecast_dict = sorted(forecast_dict.items(), key=lambda item: -len(item[1]))
+    sorted_forecast_dict = sorted(forecast_dict.items(), key=lambda x: (x[1] != 'Sunny', x[1] != 'Cloudy', x[1] != 'Rainy', x[0]))
     for key, value in sorted_forecast_dict:
         result.append(f'{key} - {value}')
-    return result
+    return '\n'.join(result)
 
+
+# print(forecast(
+#     ("Sofia", "Sunny"),
+#     ("London", "Cloudy"),
+#     ("New York", "Sunny")))
+
+# print(forecast(
+#     ("Beijing", "Sunny"),
+#     ("Hong Kong", "Rainy"),
+#     ("Tokyo", "Sunny"),
+#     ("Sofia", "Cloudy"),
+#     ("Peru", "Sunny"),
+#     ("Florence", "Cloudy"),
+#     ("Bourgas", "Sunny")))
 
 print(forecast(
-    ("Sofia", "Sunny"),
-    ("London", "Cloudy"),
-    ("New York", "Sunny")))
+    ("Tokyo", "Rainy"),
+    ("Sofia", "Rainy")))
